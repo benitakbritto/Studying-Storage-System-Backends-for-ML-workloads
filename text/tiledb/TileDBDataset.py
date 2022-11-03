989c12835164ec1fbc957125996684e9fd276f76
Hemal Patel <hemal.patel7735@outlook.com>
2022-11-03 17:04:57 -0500

    twitter(tiledb): add diff ways to fetch item from db

--- text/tiledb/TileDBDataset.py
+++ text/tiledb/TileDBDataset.py
@@ -12,12 +12,19 @@ class TileDBDataset(Dataset):
 
     def __getitem__(self, idx):
         with tiledb.open(constants.tileUri, 'r') as A:
-            tldb_row = A[idx]
+            # way 1: get the entire row
+            # tldb_row = A[idx]
+            
+            # way 2: only fetch wanted attributes
+            tldb_row = A.query(attrs=("target", "text"), coords=False, order='G')[idx]
+            # print(tldb_row)
+
             return tldb_row['target'], tldb_row['text']
 
 
 if __name__ == "__main__":
     twitter = TileDBDataset()
 
-    for i in range(0, 10):
-        print(twitter.__getitem__(i))
+    for i in range(0, 3):
+        target, text = twitter.__getitem__(i)
+        print(target, text)
