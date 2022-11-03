fbd7b913be2024bdc10419f7416cca8da9c664f0
Benita Britto <benita.k.britto@gmail.com>
2022-11-03 18:17:39 -0500

    add dataloader

--- text/rocksdb/data_loader.py
+++ text/rocksdb/data_loader.py
@@ -11,6 +11,7 @@ from torch.utils.data import Dataset, DataLoader
 import helper as bytes
 import constants
 import json
+import time
 
 class RocksDBDataset(Dataset):
     def __init__(self):
@@ -19,18 +20,30 @@ class RocksDBDataset(Dataset):
     def __len__(self):
         val = self.db.get(constants.METADATA_KEY.encode())
         assert val is not None
-        return self.bytes_to_int(val)
+        return bytes.bytes_to_int(val)
     
     # returns the text and target attribute from a row
     def __getitem__(self, idx):
         val = self.db.get(bytes.int_to_bytes(idx))
         val = json.loads(val.decode())
-        
         return val['text'], val['target']
         
 if __name__ == "__main__":
     twitter = RocksDBDataset()
+    start = time.time()
+    
+    # TODO: Tweak these values
+    data_train = torch.utils.data.DataLoader(
+        twitter,
+        batch_size=4,
+        shuffle=True, 
+        num_workers=0
+    )
 
-    for i in range(0, 3):
-        print(twitter.__getitem__(i))
+    i = 0
+    for batch_idx, samples in enumerate(data_train):
+        i = batch_idx
+    
+    end = time.time()
+    print(f'Elapsed time = {end - start}')
         
