fa57a9821e9a562f305b6a355f6f241dd69fe2de
Hemal Patel <hemal.patel7735@outlook.com>
2022-11-06 16:06:01 -0600

    twitter(tiledb): use IterableDataset instead of MapDataSet

---
+++ text/tiledb/TileDBIterator.py
@@ -0,0 +1,46 @@
+import tiledb
+import constants
+
+class TileDBIterator():
+    def __init__(self, cache_len, start, end):
+        # cache stores
+        self.target = []
+        self.text = []
+
+        self.curr_idx = start
+        self.end_idx = end
+        # idx after which we need to fetch from DB
+        self.last_exclusive_idx = start - 1
+        
+        # items to fetch in one shot, don't overfetch
+        self.cache_len = min(cache_len, end - start + 1)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.curr_idx >= self.end_idx:
+            print('raising ex:', self.curr_idx)
+            raise StopIteration
+
+        # pre-fetch
+        elif self.curr_idx >= self.last_exclusive_idx:
+            # print('fetching:', self.curr_idx)
+            with tiledb.open(constants.tileUri, 'r') as A:
+                # find last index to fetch(exclusive)
+                self.last_exclusive_idx = min(self.curr_idx + self.cache_len, self.end_idx)
+
+                # bulk fetch
+                data = A.query(attrs=("target", "text"), coords=False, order='G')[self.curr_idx: self.last_exclusive_idx]
+                
+                # cache store
+                self.target = data['target']
+                self.text = data['text']
+                
+        # find the relative index within cache data
+        relative_idx = self.curr_idx % len(self.target)
+        
+        # advance to next index
+        self.curr_idx = self.curr_idx + 1
+
+        return self.target[relative_idx], self.text[relative_idx]
