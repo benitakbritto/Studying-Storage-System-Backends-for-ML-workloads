7f28118fd8c2b5847bc03db32bc82c86f79aa6b9
Benita Britto <benita.k.britto@gmail.com>
2022-12-07 14:24:38 -0600

    add folder script

---
+++ scripts/make_output_folders.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+mkdir -p ../output/rd/text
+mkdir -p ../output/rd/image
+mkdir -p ../output/rd/graph
+
+mkdir -p ../output/td/text
+mkdir -p ../output/td/image
+mkdir -p ../output/td/graph
+
+mkdir -p ../output/ts/text
+mkdir -p ../output/ts/image
+mkdir -p ../output/ts/graph
