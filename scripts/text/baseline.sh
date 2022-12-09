ecf1779200c8fa38fcc80dead7d1390d11bc67d9
reetu <reetuparna.1988@gmail.com>
2022-12-09 17:21:42 -0600

    uncommented code

--- scripts/text/baseline.sh
+++ scripts/text/baseline.sh
@@ -11,20 +11,20 @@ TYPE=m
 DS=base
 ROWSPERKEY=1
 
-# Map style
-# for workers in 0 8 16 32
-# do 
-#     WORKERS=$workers
-#     for batchsize in 128 256 512 1024
-#     do
-#         BATCHSIZE=$batchsize
-#         OUTPUTFILE="../output/${DS}/${WORKLOAD}/i${INPUTFILESIZE}_w${WORKERS}_r${ROWSPERKEY}_t${TYPE}_b${BATCHSIZE}"
+Map style
+for workers in 0 8 16 32
+do 
+    WORKERS=$workers
+    for batchsize in 128 256 512 1024
+    do
+        BATCHSIZE=$batchsize
+        OUTPUTFILE="../output/${DS}/${WORKLOAD}/i${INPUTFILESIZE}_w${WORKERS}_r${ROWSPERKEY}_t${TYPE}_b${BATCHSIZE}"
         
-#         echo "${BLUE} DS=${DS}, WORKLOAD=${WORKLOAD}, WORKERS=${WORKERS}, TYPE=${TYPE}, ROWSPERKEY=${ROWSPERKEY}, BATCHSIZE=${BATCHSIZE} ${NOCOLOR}"
-#         python ../$WORKLOAD/main.py  -input-file $INPUTFILE -num-workers $WORKERS -batch-size $BATCHSIZE > $OUTPUTFILE
+        echo "${BLUE} DS=${DS}, WORKLOAD=${WORKLOAD}, WORKERS=${WORKERS}, TYPE=${TYPE}, ROWSPERKEY=${ROWSPERKEY}, BATCHSIZE=${BATCHSIZE} ${NOCOLOR}"
+        python ../$WORKLOAD/main.py  -ds $DS -type m -input-file $INPUTFILE -num-workers $WORKERS -batch-size $BATCHSIZE > $OUTPUTFILE
         
-#     done
-# done
+    done
+done
 
 #TODO Remove this file, use it only to run baseline tests separately
 
@@ -41,6 +41,6 @@ for batchsize in 128 256 512 1024
         
         echo "${BLUE} DS=${DS}, WORKLOAD=${WORKLOAD}, WORKERS=${WORKERS}, TYPE=${TYPE}, ROWSPERKEY=${ROWSPERKEY}, BATCHSIZE=${BATCHSIZE} ${NOCOLOR}"
         
-        python ../$WORKLOAD/main.py -ds $DS -input-file $INPUTFILE -type $TYPE -batch-size $BATCHSIZE  > $OUTPUTFILE
+        python ../$WORKLOAD/main.py -ds $DS -type m -input-file $INPUTFILE -type $TYPE -batch-size $BATCHSIZE  > $OUTPUTFILE
             
     done
