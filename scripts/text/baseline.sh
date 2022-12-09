a8a768da2f06173c04070de3409aaf592a051eff
reetu <reetuparna.1988@gmail.com>
2022-12-09 14:34:48 -0600

    typo fix

--- scripts/text/baseline.sh
+++ scripts/text/baseline.sh
@@ -5,26 +5,26 @@ NOCOLOR='\033[0m'
 # Constants
 INPUTFILE=/mnt/data/dataset/twitter/twitter_sentiment_dataset.csv
 ITR=1
-WORKLOAD=TEXT
+WORKLOAD=text
 WRITE=1
 TYPE=m
 DS=base
 ROWSPERKEY=1
 
 # Map style
-for workers in 0 8 16 32
-do 
-    WORKERS=$workers
-    for batchsize in 128 256 512 1024
-    do
-        BATCHSIZE=$batchsize
-        OUTPUTFILE="../output/${DS}/${WORKLOAD}/i${INPUTFILESIZE}_w${WORKERS}_r${ROWSPERKEY}_t${TYPE}_b${BATCHSIZE}"
+# for workers in 0 8 16 32
+# do 
+#     WORKERS=$workers
+#     for batchsize in 128 256 512 1024
+#     do
+#         BATCHSIZE=$batchsize
+#         OUTPUTFILE="../output/${DS}/${WORKLOAD}/i${INPUTFILESIZE}_w${WORKERS}_r${ROWSPERKEY}_t${TYPE}_b${BATCHSIZE}"
         
-        echo "${BLUE} DS=${DS}, WORKLOAD=${WORKLOAD}, WORKERS=${WORKERS}, TYPE=${TYPE}, ROWSPERKEY=${ROWSPERKEY}, BATCHSIZE=${BATCHSIZE} ${NOCOLOR}"
-        python ../$WORKLOAD/baseline_map.py  -input-file $INPUTFILE -num-workers $WORKERS -batch-size $BATCHSIZE > $OUTPUTFILE
+#         echo "${BLUE} DS=${DS}, WORKLOAD=${WORKLOAD}, WORKERS=${WORKERS}, TYPE=${TYPE}, ROWSPERKEY=${ROWSPERKEY}, BATCHSIZE=${BATCHSIZE} ${NOCOLOR}"
+#         python ../$WORKLOAD/main.py  -input-file $INPUTFILE -num-workers $WORKERS -batch-size $BATCHSIZE > $OUTPUTFILE
         
-    done
-done
+#     done
+# done
 
 #TODO Remove this file, use it only to run baseline tests separately
 
@@ -41,6 +41,6 @@ for batchsize in 128 256 512 1024
         
         echo "${BLUE} DS=${DS}, WORKLOAD=${WORKLOAD}, WORKERS=${WORKERS}, TYPE=${TYPE}, ROWSPERKEY=${ROWSPERKEY}, BATCHSIZE=${BATCHSIZE} ${NOCOLOR}"
         
-        python ../$WORKLOAD/baseline_iterable.py -ds $DS -input-file $INPUTFILE -type $TYPE -batch-size $BATCHSIZE  > $OUTPUTFILE
+        python ../$WORKLOAD/main.py -ds $DS -input-file $INPUTFILE -type $TYPE -batch-size $BATCHSIZE  > $OUTPUTFILE
             
     done
