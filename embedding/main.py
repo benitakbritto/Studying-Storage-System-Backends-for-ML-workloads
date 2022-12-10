a546872cf9aca06932d862d8b8f6aea01ac6a69f
Hemal Patel <hemal.patel7735@outlook.com>
2022-12-10 00:55:59 -0600

    embedding(graph): script

--- embedding/main.py
+++ embedding/main.py
@@ -73,7 +73,7 @@ def gen_emb(size):
 # r_store = RocksDBEmbedding()
 # tile_store = TileDBEmbedding(rows_count=max_number, cols_count=emb_size, tile_uri=get_tile_uri(type))
 
-ds_list = ['ts']
+ds_list = ['rd','td','ts']
 
 if args.ds != 'all':
     ds_list = [args.ds]
--- scripts/embedding/graph.sh
+++ scripts/embedding/graph.sh
@@ -1,3 +1,30 @@
-#!/bin/sh
+BLUE='\033[0;44m'
+NOCOLOR='\033[0m'
 
-echo "Running Graph"
+# Constants
+WORKLOAD=graph
+DS=td
+INPUT_FILE=/mnt/data/embeddings/graph.txt
+TYPE=power
+DS_SIZE=1024
+DS_SIZE_LIMIT=2048
+EMBED_SIZE_LIMIT=400
+BATCH_SIZE_LIMIT=1024
+
+while [ $DS_SIZE -le $DS_SIZE_LIMIT ]
+do 
+    EMBED_SIZE=100
+    while [ $EMBED_SIZE -le $EMBED_SIZE_LIMIT ]
+    do
+        BATCH_SIZE=128
+        while [ $BATCH_SIZE -le $BATCH_SIZE_LIMIT ]
+        do
+            OUTPUTFILE="./output/${WORKLOAD}/d${DS_SIZE}_e${EMBED_SIZE}_b${BATCH_SIZE}"
+            echo "${BLUE} WORKLOAD=${WORKLOAD}, ds_size=${DS_SIZE}, EMBED_SIZE=${EMBED_SIZE}, BATCH_SIZE=${BATCH_SIZE} ${NOCOLOR}"
+            python ../../embedding/main.py -ds-size $DS_SIZE -ds $DS -batch-size $BATCH_SIZE -embed-size $EMBED_SIZE -input-file $INPUT_FILE -type $TYPE > $OUTPUTFILE
+        BATCH_SIZE=$((BATCH_SIZE * 2))
+        done
+    EMBED_SIZE=$((EMBED_SIZE + 100))
+    done
+    DS_SIZE=$((DS_SIZE * 2))
+done
